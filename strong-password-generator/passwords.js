74215c42be8a34a10a5aeafdf96a6077ec3ae932
Raquel <raquelsglez99@gmail.com>
2024-04-02 21:41:42 +0200

    feat:create page strong passwword

---
+++ strong-password-generator/passwords.js
@@ -0,0 +1,34 @@
+const strongPassword = document.getElementById('strong-password');
+const buttonPassword = document.getElementById('button-password');
+const errorMessage = document.getElementById("error-message");
+const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+const numberChars = "0123456789";
+const symbolChars = "!@#$%^&*()-_=+";
+const allChars = uppercaseChars + lowercaseChars + numberChars + symbolChars;
+
+function generatePassword() {
+    const lengthPasswordValue = parseInt(document.getElementById('length-password').value);
+
+
+    if (lengthPasswordValue<12 || lengthPasswordValue>50){
+        errorMessage.style.display = "block";
+        strongPassword.innerHTML = "";
+        return
+    }
+
+    errorMessage.style.display = "none";
+    let password = "";
+    password += uppercaseChars[(Math.floor(Math.random() * uppercaseChars.length))];
+    password += lowercaseChars[(Math.floor(Math.random() * lowercaseChars.length))];
+    password += numberChars[(Math.floor(Math.random() * numberChars.length))];
+    password += symbolChars[(Math.floor(Math.random() * symbolChars.length))];
+
+    for(let i = password.length; i < lengthPasswordValue; i++) {
+        password += allChars[(Math.floor(Math.random() * allChars.length))];
+    }
+
+   strongPassword.innerHTML = password;
+}
+
+buttonPassword.addEventListener('click', generatePassword);
